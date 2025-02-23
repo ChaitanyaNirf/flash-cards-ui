671103746c4bd5b3e4b48c8338f9b095ee49260f
chaitanyaNirf <chaitunirfarake@gmail.com>
2025-02-23 11:51:29 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'flashCardsFrontEnd';
+  constructor() {
+    if(localStorage.getItem('theme') === 'dark'){
+      document.body.classList.add('dark-theme');
+    }
+  }
 }
